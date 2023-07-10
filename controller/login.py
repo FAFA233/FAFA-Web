e3e87173bc15269e81e11e04181ef226f9e3071f
Atlzf <1924855952@qq.com>
2023-07-10 19:34:00 +0800

    update

--- controller/login.py
+++ controller/login.py
@@ -1,21 +1,21 @@
 from flask import Flask, request, jsonify
-from model.user import user
+from model.user import User
 app = Flask(__name__)
 
 @app.route('/login', methods=['POST'])#'/login'为url
-class user_information:
+class user_info:
     username = request.form.get('username')
     password = request.form.get('password')#从前端获取账号密码
 
 def login():
 
-    if(user.login(user_information.username,user_information.password)==True):
+    if(User.login(user_info.username,user_info.password)==True):
         return "Login successful"  # 返回登录成功的提示信息
     else:
         return "Login failed"
 
 def register():
-    if(user.register(user_information.username and user_information.password)):
+    if(User.register(user_info.username , user_info.password)):
         return "registe successfully"
     
 if __name__ == '__main__':
