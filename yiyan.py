0570ff07f58ed863c52f8ada067a7b5a6bf42a7a
Atlzf <1924855952@qq.com>
2023-07-11 09:49:55 +0800

    一言

--- yiyan.py
+++ yiyan.py
@@ -1,6 +1,21 @@
 import requests
 import json
-url = 'https://v1.hitokoto.cn'
-js= requests.get(url)
-l_str=json.loads(js.text)
-print(l_str['hitokoto'])
+from flask import Flask, jsonify
+
+app=Flask(__name__)
+@app.route('/display_json')#url
+
+def print_json():
+    """
+        从一言获取json
+    Returns:
+        json里的名言部分
+    """
+    url = 'https://v1.hitokoto.cn'
+    js= requests.get(url)
+    l_str=json.loads(js.text)
+    return l_str['hitokoto']
+    
+
+if __name__ == '__main__':
+    app.run()
