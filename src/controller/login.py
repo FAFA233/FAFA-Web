6e23b63ab0605e0a890fe2c4ff727e33febb6941
Atlzf <1924855952@qq.com>
2023-07-11 16:57:28 +0800

    update

---
+++ src/controller/login.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+from src.model import User
+
+app = Flask(__name__)
+
+@app.route('/login', methods=['POST'])
+def login():
+    if username := request.form.get('username') is None:
+        username = ""
+    #获取用户名和密码
+    if password := request.form.get('password') is None:
+        password = ""
+    
+    if User.login(username, password):
+        return jsonify({'message': 'Login successful'})#返回json登陆成功
+    else:
+        return jsonify({'message': 'Login failed'})
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    
+    if User.register(username, password):
+        return jsonify({'message': 'Register successful'})
+    else:
+        return jsonify({'message': 'Register failed'})
+
+@app.route('/forgotton_password', methods=['POST'])
+def forgotton_password():
+    username =request.form.get('username')
+    new_password = request.form.get('new_password')
+
+    if User.forgotton_password(username,new_password):
+        return jsonify ({'message': 'Changing successful'})
+    else:
+        return jsonify({'message': 'changing successful'})
+    
+if __name__ == '__main__':#run！
+    app.run()
