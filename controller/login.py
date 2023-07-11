b6d1735cc6bbe047d0ed97df8b5cf1a5ac325ce8
Atlzf <1924855952@qq.com>
2023-07-11 11:18:02 +0800

    update

--- controller/login.py
+++ controller/login.py
@@ -2,21 +2,35 @@ from flask import Flask, request, jsonify
 from model.user import User
 app = Flask(__name__)
 
-@app.route('/login', methods=['POST'])#'/login'为url
-class user_info:
-    username = request.form.get('username')
-    password = request.form.get('password')#从前端获取账号密码
-
+@app.route('/login', methods=['POST'])
 def login():
-
-    if(User.login(user_info.username,user_info.password)==True):
-        return "Login successful"  # 返回登录成功的提示信息
-    else:
-        return "Login failed"
-
-def register():
-    if(User.register(user_info.username , user_info.password)):
-        return "registe successfully"
+    username = request.form.get('username')#获取用户名和密码
+    password = request.form.get('password')
     
-if __name__ == '__main__':
+    if User.login(username, password):
+        return jsonify({'message': 'Login successful'})#返回json登陆成功
+    else:
+        return jsonify({'message': 'Login failed'})
+
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    
+    if User.register(username, password):
+        return jsonify({'message': 'Register successful'})
+    else:
+        return jsonify({'message': 'Register failed'})
+
+@app.route('/forgotton_password', methods=['POST'])
+def forgotton_password():
+    username =request.form.get('username')
+    new_password = request.form.get('new_password')
+
+    if User.forgotton_password(username,new_password):
+        return jsonify ({'message': 'Changing successful'})
+    else:
+        return jsonify({'message': 'changing successful'})
+    
+if __name__ == '__main__':#run！
     app.run()
