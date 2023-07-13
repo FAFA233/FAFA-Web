80a97c66b481ae3f821199d6a8a8a6f81b70e9fc
Atlzf <1924855952@qq.com>
2023-07-13 19:37:20 +0800

    update

--- articles/article.py
+++ articles/article.py
@@ -1,21 +1,7 @@
 import time
-class ArticleModel:
-    def __init__(self, name, author_name=""):
-        self.name = name
-        self.author_name = author_name
-        self.publish_date = time.localtime()
-        self.latest_edited_date = time.localtime()
-        self.file_name: str
-
-    def update(self):
-        self.latest_edited_date = time.localtime()
+class article_model:
+    def __init__(self):
+      
         
-    def delete(self):
-        self.name = ""
-        self.author_name = ""
-        self.publish_date = None
-        self.latest_edited_date = None
-        self.file_name = ""
-    
 
     
--- articles/file.py
+++ articles/file.py
@@ -1,16 +1,2 @@
 class ArticleController:
-    #不知道自己写了啥玩意
-    def __init__(self, model):
-        self.model = model
-
-    def update_article(self):
-        self.model.update()
-
-    def delete_article(self):
-        self.model.delete()
-
-    def set_article_name(self, name):
-        self.model.name = name
-
-    def set_article_author(self, author_name):
-        self.model.author_name = author_name
+    
